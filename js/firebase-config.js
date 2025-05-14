82d88de6b266517543cd409e943719497a54038f
xoenfalos2014 <XOENFALOS2014@GMAIL.COM>
2025-05-14 20:59:21 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOc-qReOgBoDWQOYVNSlNMgRjF_hfH6uw",
+  authDomain: "sistema-chamados-99e49.firebaseapp.com",
+  projectId: "sistema-chamados-99e49",
+  storageBucket: "sistema-chamados-99e49.firebasestorage.app",
+  messagingSenderId: "680351942005",
+  appId: "1:680351942005:web:896b35a40f094d42297f3c"
+};
+
+firebase.initializeApp(firebaseConfig);
