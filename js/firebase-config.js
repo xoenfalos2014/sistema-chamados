48318ab7f6d7e4c3755c216883923301423ab4f7
xoenfalos2014 <XOENFALOS2014@GMAIL.COM>
2025-05-16 17:38:34 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+
+// Substitua os valores abaixo pelas suas credenciais reais do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
